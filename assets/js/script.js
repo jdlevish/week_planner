34f2125a557ac75630d5f0a9933ee5badda45800
jdlevish <45375460+jdlevish@users.noreply.github.com>
2020-06-14 21:54:27 -0700

    moved all variable declarations into functions and commented code thouroghly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,10 @@
-// global variables
-var dateDisplay = $("#currentDay");
-var rows = $(".row");
-var inputs = $(".input");
 // function that handles the color coding of the hour rows
 function colorChange() {
+	var rows = $(".row");
+	var dateDisplay = $("#currentDay");
 	dateDisplay.text(moment().format("dddd, MMMM Do YYYY, h:mm a"));
 
 	for (i = 0; i < rows.length; i++) {
-		console.log(
-			parseInt(rows[i].dataset.value) < parseInt(moment().format("HH"))
-		);
-		console.log();
 		if (parseInt(rows[i].dataset.value) > parseInt(moment().format("HH"))) {
 			$(rows[i]).addClass("future");
 		} else if (
@@ -24,7 +18,7 @@ function colorChange() {
 		}
 	}
 }
-// function to save textarea to localStorage
+// this function saves the text entered into the textarea inputs to local storage when the save button for that block is clicked
 function saveInput() {
 	$("button").on("click", function () {
 		event.preventDefault(event);
@@ -36,10 +30,11 @@ function saveInput() {
 		);
 	});
 }
-
+// this function checks local storage to see if an object has been previously stored for each hour block
+// if it finds an object associated with that block it displays the value in the text area
 function checkStorage() {
+	var inputs = $(".input");
 	for (i = 0; i < inputs.length; i++) {
-		console.log(inputs[i]);
 		if (localStorage.getItem(inputs[i].dataset.value) != null) {
 			inputs[i].value = localStorage.getItem(inputs[i].dataset.value);
 		}
@@ -48,6 +43,5 @@ function checkStorage() {
 $(document).ready(function () {
 	colorChange();
 	saveInput();
-
 	checkStorage();
 });
